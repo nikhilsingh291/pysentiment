9c8aa4c960acba379f2703b711d1d9ece900a81b
Nikhil Singh <nikhilsingh291@gmail.com>
2017-12-06 18:28:37 +0530

    twittersent.py added

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # pysentiment
 Sentiment Analysis of data from different sources
+
+
+fetchtweet.py
+
+Python script to fetch tweets using Twitter api via tweepy module.
+
+
+
+fetchtweet.py
+
+Gives sentiment analysis of the search query by fetching tweets from twitter.
+Analysis is done using Texblob.
---
+++ fetchtweet.py
@@ -0,0 +1,57 @@
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import json
+import tweepy
+#consumer key, consumer secret, access token, access secret.
+ckey="jhCHKQUQh53XsZTE1qDAEpT7A"
+csecret="ZsrVH2oVxhVHJd7CHTXvBHOV9TSjDyWrG0S9KN8W8FTRLFCFFs"
+atoken="4826492292-PjuzIpowGQYRfEopeUnWfdXr6xZizULPSSi4NBb"
+asecret="9UsyHvA6QlrhZSvYVqXljwmuWvyPXnIGr4Zl3XnONOlk3"
+f=open('gagloc.txt','w')
+##
+class listener(StreamListener):
+   
+
+    def on_data(self, data):
+        all_data=json.loads(data)
+        savefile=open('cg.csv','a')
+        savefile.write(data)
+        savefile.write('\n')
+        savefile.close()
+##        #print len(all_data)
+        #print(all_data["user"]["screen_name"])
+        print(all_data["text"])
+        print(all_data["user"]["location"])
+##        f=open('gagloc.txt','w')
+##        f.write(all_data["user"]["location"])
+##        f.close()
+        return(True)
+    def on_error(self, status):
+        print status
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+
+api=tweepy.API(auth)
+##user = api.me()
+ 
+##print('Name: ' + user.name)
+##print('Location: ' + user.location)
+##print('Friends: ' + str(user.friends_count))
+
+##api.update_status('Hola Twitter 2, (checking twitter python api)')
+##
+twitterStream = Stream(auth, listener())
+twitterStream.filter(track=["gddindia"])
+print twitterStream
+
+
+
+
+
+
+
+
+
+
